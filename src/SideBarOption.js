dcb2f0cec35ca8db82afb8d7eab137fd1e0dc287
dineshmsd051 <dineshmsd051@gmail.com>
2023-07-06 00:33:12 +0530

    Added sidebar style fixes

--- src/SideBarOption.js
+++ src/SideBarOption.js
@@ -3,11 +3,13 @@ import "./SideBarOption.css";
 
 function SideBarOption({ active, text, Icon }) {
   return (
-    <div className={`sideBarOption ${active && `sideBarOption--active`}`}>
+    <div className="sideBarOption__Wrapper">
+      <div className={`sideBarOption ${active && `sideBarOption--active`}`}>
         <Icon />
         <h2>{text}</h2>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SideBarOption

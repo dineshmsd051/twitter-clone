2473934d35f7ec02e98f5ea606f04fa94db12f0a
dineshmsd051 <dineshmsd051@gmail.com>
2023-07-14 18:16:17 +0530

    Added firebase, posts collection

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Feed.css";
 import TweetBox from './TweetBox';
 import Post from './Post';
+import db from "./firebase"
+import {
+  doc,
+  onSnapshot,
+  collection,
+  query,
+  where
+} from "firebase/firestore";
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+
+
+useEffect(() => {
+  const postsRef = query(collection(db, "posts"));
+  const unsubscribe = onSnapshot(postsRef, (snapshot) => {
+    const posts = snapshot.docs.map((snapshotData) => snapshotData.data());
+    setPosts(posts);
+  });
+
+  return () => unsubscribe();
+}, []);
+
+
   return (
     <div className="feed">
       <div className="feed__header">
@@ -11,15 +33,18 @@ function Feed() {
       </div>
 
       <TweetBox />
-
-      <Post
-        avatar="https://lh3.googleusercontent.com/ogw/AGvuzYb8DRiIPejMPPcjgAi4JL_RH99W0ZXmSfZLAwwkNw=s32-c-mo"
-        displayName="Dinesh Msd"
-        userName="dineshmsd051"
-        isVerified={true}
-        text="my first tweet"
-        image="https://images.unsplash.com/photo-1682685796965-9814afcbff55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-      />
+      {posts?.map((post) => (
+        <div key={post.id}>
+          <Post
+            avatar={post.avatar}
+            displayName={post.displayName}
+            userName={post.userName}
+            isVerified={post.isVerified}
+            text={post.text}
+            image={post.image}
+          />
+        </div>
+      ))}
     </div>
   );
 }
